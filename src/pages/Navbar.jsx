c344e7fe54895287fa6f3f6aada2a2531d8952fd
Abdul Mannan Khan <abdmankhan@gmail.com>
2024-11-28 16:26:37 +0530

    hindi 2 eng

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
-                खोया पाया तूने है क्या ??
+                खोया-पाया ??
               </span>
             </NavLink>
             <div className="md:ml-auto">
